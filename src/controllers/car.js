17c05229955b3024c368fa7875340a2028b5a6b2
batumzff <batuhanmuzafferoglu@gmail.com>
2024-04-03 23:46:41 +0300

    reservation update

--- src/controllers/car.js
+++ src/controllers/car.js
@@ -1,16 +1,15 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 // Car Controller:
 
-const Car = require('../models/car')
-const Reservation = require('../models/reservation')
+const Car = require("../models/car");
+const Reservation = require("../models/reservation");
 
 module.exports = {
-
-    list: async (req, res) => {
-        /*
+  list: async (req, res) => {
+    /*
             #swagger.tags = ["Cars"]
             #swagger.summary = "List Cars"
             #swagger.description = `
@@ -24,65 +23,66 @@ module.exports = {
             `
         */
 
-        // Musait olmayan araçları listeleme:
-        let customFilter = { isAvailable: true }
+    // Musait olmayan araçları listeleme:
+    let customFilter = { isAvailable: true };
 
-        /* TARIHE GÖRE LİSTELE */
+    /* TARIHE GÖRE LİSTELE */
 
-        // List by dateFilter:
-        // URL?startDate=2024-01-01&endDate=2024-01-10
-        const { startDate: getStartDate, endDate: getEndDate } = req.query
+    // List by dateFilter:
+    // URL?startDate=2024-01-01&endDate=2024-01-10
+    const { startDate: getStartDate, endDate: getEndDate } = req.query;
 
-        if (getStartDate && getEndDate) {
+    if (getStartDate && getEndDate) {
+      // Belirtilen tarihlerde reserve edilmiş araçları getir:
+      const reservedCars = await Reservation.find(
+        {
+          $nor: [
+            { startDate: { $gt: getEndDate } }, // gt: >
+            { endDate: { $lt: getStartDate } }, // lt: <
+          ],
+        },
+        { _id: 0, carId: 1 }
+      ).distinct("carId");
+      // console.log(reservedCars)
 
-            // Belirtilen tarihlerde reserve edilmiş araçları getir:
-            const reservedCars = await Reservation.find({
-                $nor: [
-                    { startDate: { $gt: getEndDate } }, // gt: >
-                    { endDate: { $lt: getStartDate } } // lt: <
-                ]
-            }, { _id: 0, carId: 1 }).distinct('carId')
-            // console.log(reservedCars)
+      // Gelen Data:
+      // [
+      //     { carId: new ObjectId("660d9d2932ba8b3174a05721") },
+      //     { carId: new ObjectId("660d9d2932ba8b3174a05721") }
+      // ]
+      // convert to Filtre Data (distinct);
+      // [
+      //    new ObjectId("660d9d2932ba8b3174a05721"),
+      //    new ObjectId("660d9d2932ba8b3174a05721")
+      // ]
 
-            // Gelen Data:
-            // [
-            //     { carId: new ObjectId("660d9d2932ba8b3174a05721") },
-            //     { carId: new ObjectId("660d9d2932ba8b3174a05721") }
-            // ]
-            // convert to Filtre Data (distinct);
-            // [
-            //    new ObjectId("660d9d2932ba8b3174a05721"),
-            //    new ObjectId("660d9d2932ba8b3174a05721")
-            // ]
+      // Filter objesine NotIN (nin) ekle:
+      if (reservedCars.length) {
+        customFilter._id = { $nin: reservedCars };
+      }
+      // console.log(customFilter)
+    } else {
+      req.errorStatusCode = 401;
+      throw new Error("startDate and endDate queries are required.");
+    }
 
-            // Filter objesine NotIN (nin) ekle:
-            if (reservedCars.length) {
-                customFilter._id = { $nin: reservedCars }
-            }
-            console.log(customFilter)
-        
-        } else {
-            req.errorStatusCode = 401
-            throw new Error('startDate and endDate queries are required.')
-        }
+    /* TARIHE GÖRE LİSTELE */
 
-        /* TARIHE GÖRE LİSTELE */
+    // const data = await res.getModelList(Car, { _id: { $nin: ['carid12345667', 'carid12345667']} } )
+    const data = await res.getModelList(Car, customFilter, [
+      { path: "createdId", select: "username" },
+      { path: "updatedId", select: "username" },
+    ]);
 
-        // const data = await res.getModelList(Car, { _id: { $nin: ['carid12345667', 'carid12345667']} } )
-        const data = await res.getModelList(Car, customFilter, [
-            { path: 'createdId', select: 'username' },
-            { path: 'updatedId', select: 'username' },
-        ])
+    res.status(200).send({
+      error: false,
+      details: await res.getModelListDetails(Car, customFilter),
+      data,
+    });
+  },
 
-        res.status(200).send({
-            error: false,
-            details: await res.getModelListDetails(Car, customFilter),
-            data
-        })
-    },
-
-    create: async (req, res) => {
-        /*
+  create: async (req, res) => {
+    /*
             #swagger.tags = ["Cars"]
             #swagger.summary = "Create Car"
             #swagger.parameters['body'] = {
@@ -94,38 +94,37 @@ module.exports = {
             }
         */
 
-        // createdId ve updatedId verisini req.user'dan al:
-        req.body.createdId = req.user._id
-        req.body.updatedId = req.user._id
+    // createdId ve updatedId verisini req.user'dan al:
+    req.body.createdId = req.user._id;
+    req.body.updatedId = req.user._id;
 
-        const data = await Car.create(req.body)
+    const data = await Car.create(req.body);
 
-        res.status(201).send({
-            error: false,
-            data
-        })
-    },
+    res.status(201).send({
+      error: false,
+      data,
+    });
+  },
 
-    read: async (req, res) => {
-        /*
+  read: async (req, res) => {
+    /*
             #swagger.tags = ["Cars"]
             #swagger.summary = "Get Single Car"
         */
 
-        const data = await Car.findOne({ _id: req.params.id }).populate([
-            { path: 'createdId', select: 'username' },
-            { path: 'updatedId', select: 'username' },
-        ])
+    const data = await Car.findOne({ _id: req.params.id }).populate([
+      { path: "createdId", select: "username" },
+      { path: "updatedId", select: "username" },
+    ]);
 
-        res.status(200).send({
-            error: false,
-            data
-        })
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
 
-    },
-
-    update: async (req, res) => {
-        /*
+  update: async (req, res) => {
+    /*
             #swagger.tags = ["Cars"]
             #swagger.summary = "Update Car"
             #swagger.parameters['body'] = {
@@ -137,31 +136,31 @@ module.exports = {
             }
         */
 
-        // updatedId verisini req.user'dan al:
-        req.body.updatedId = req.user._id
+    // updatedId verisini req.user'dan al:
+    req.body.updatedId = req.user._id;
 
-        const data = await Car.updateOne(customFilter, req.body, { runValidators: true })
+    const data = await Car.updateOne(customFilter, req.body, {
+      runValidators: true,
+    });
 
-        res.status(202).send({
-            error: false,
-            data,
-            new: await Car.findOne({ _id: req.params.id })
-        })
+    res.status(202).send({
+      error: false,
+      data,
+      new: await Car.findOne({ _id: req.params.id }),
+    });
+  },
 
-    },
-
-    delete: async (req, res) => {
-        /*
+  delete: async (req, res) => {
+    /*
             #swagger.tags = ["Cars"]
             #swagger.summary = "Delete Car"
         */
 
-        const data = await Car.deleteOne({ _id: req.params.id })
+    const data = await Car.deleteOne({ _id: req.params.id });
 
-        res.status(data.deletedCount ? 204 : 404).send({
-            error: !data.deletedCount,
-            data
-        })
-
-    },
-}
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
+      data,
+    });
+  },
+};
